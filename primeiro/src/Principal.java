cb4a2f5933683b75c807abeb3602dd41182ec7c7
mdsavassini <mdsavassini@yahoo.com>
2022-06-02 12:59:40 -0300

    Primeiro Projeto Java

---
+++ primeiro/src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int a = 10;
+        int b = 10;
+        int c = a+b;
+
+        System.out.println("A Soma de a + b é igual a: ");
+        System.out.println(c) ;
+    }
+}
