49383fa293158db30e83fc428cb64eac619ba8c5
mdsavassini <mdsavassini@yahoo.com>
2022-06-07 15:39:25 -0300

    ExercicioIfSwitch

--- DIO/src/main.java
+++ DIO/src/main.java
@@ -20,5 +20,26 @@ public class main {
         Emprestimo.calcular(1000,2);
         Emprestimo.calcular(1000,3);
         Emprestimo.calcular(1000,5);
+
+
+        // Quadrilatero
+        System.out.println("Exercicios do Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+
+
+        // QuadrilateroReturn
+        System.out.println("Exercicio Quadrilatero return");
+
+        double areaQuadrado = QuadrilateroRetorn.area(3);
+        System.out.println("Área do quadrado:"+ areaQuadrado);
+
+        double areaRetangulo = QuadrilateroRetorn.area(5,5);
+        System.out.println("Área do retangulo:" + areaRetangulo );
+
+        double areaTrapezio = QuadrilateroRetorn.area(7,8,9);
+        System.out.println("Área do Trapezio:" + areaTrapezio);
+
     }
 }
